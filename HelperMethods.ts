1e0514a36d099bbb8d597f35023ca8106c4adf5a
Andrei Stoica <andreistoica4p@gmail.com>
2024-04-30 11:21:48 +0300

    consent coockies pop up solution

--- HelperMethods.ts
+++ HelperMethods.ts
@@ -2,7 +2,8 @@ import { Locator, Page } from "@playwright/test";
 
 export class HelperMethods {
   readonly page: Page;
-
+  readonly dataUseConsentLocator: string = '//p[text()="Consent"]'
+  
   constructor(page: Page) {
     this.page = page;
   }
@@ -13,5 +14,15 @@ export class HelperMethods {
 
   async fillField(locator: string, text: string) {
     await this.page.fill(locator, text);
+  }
+
+  async acceptCookies() {
+    try {
+      console.log("Accepting cookies");
+      await this.page.waitForSelector(this.dataUseConsentLocator, { timeout: 3000 });
+      await this.page.locator(this.dataUseConsentLocator).click();
+    } catch (TimeoutError) {
+      console.log("No cookies to accept.");
+    }
   }
 }
--- UI/page_object/HomePage.ts
+++ UI/page_object/HomePage.ts
@@ -4,14 +4,12 @@ export class HomePage {
   readonly page: Page;
   readonly subtitleLocator: Locator;
   readonly loginLocator: string;
-  readonly newUserTextLocator: Locator;
-  readonly dataUseConsentLocator: string;
+  readonly newUserTextLocator: Locator;  
 
   constructor(page: Page) {
     this.page = page;
     this.subtitleLocator = this.page.locator("#slider-carousel > div > div:first-child > div > h2");
     this.loginLocator = "a[href*='login']";
-    this.newUserTextLocator = this.page.locator('//h2[text()="New User Signup!"]');
-    this.dataUseConsentLocator = '//p[text()="Consent"]';
+    this.newUserTextLocator = this.page.locator('//h2[text()="New User Signup!"]');    
   }
 }
